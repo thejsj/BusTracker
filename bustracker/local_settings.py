232267220b91366d52b5b8da2447b426c2f0f106
thejsj <jorge.silva@thejsj.com>
2013-12-25 19:05:14 -0500

    Removing local settings form repo

--- bustracker/local_settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-APP_DICRECOTRY = os.path.dirname(os.path.dirname(__file__))
-
-STATICFILES_DIRS = (
-    os.path.join(APP_DICRECOTRY, 'static'),
-    os.path.join(APP_DICRECOTRY, 'media'),
-    '/Users/jorgesilva/Sites/2013/BusTracker/static',
-)
